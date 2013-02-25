08cad75364dd8f82eb2128ee8e45ce582235752c
kagemusha <mmadrid99@gmail.com>
2013-02-25 18:09:10 -0500

    added config.assets.initialize_on_precompile = false to app.rb for heroku

--- config/application.rb
+++ config/application.rb
@@ -42,6 +42,6 @@ module Crambear
 
     # Configure sensitive parameters which will be filtered from the log file.
     config.filter_parameters += [:password]
-
+    config.assets.initialize_on_precompile = false
   end
 end
