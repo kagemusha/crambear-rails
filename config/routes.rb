10e3b1a7cf5b354d893dfda4f15754eb23a5e190
kagemusha <mmadrid99@gmail.com>
2013-03-12 22:43:11 -0400

    added resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,15 @@ Crambear::Application.routes.draw do
 
   devise_for :users
 
+  resources :card_sets
+  resources :cards
+  resources :labels
+  resources :results
+  resources :card_sets do
+    resources :cards
+    resources :labels
+  end
+
   root :to => 'home#index'
 
 end
