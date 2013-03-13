504d71a3c2a3405d308f1c79ac1e74a088c168a0
kagemusha <mmadrid99@gmail.com>
2013-03-12 22:38:06 -0400

    basic data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,4 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.create! :name => "TestUser", :email => "test@test.com", :password => "tester", :password_confirmation => "tester"
+user = User.create! :name => "TestUser", :email => "test@test.com", :password => "tester", :password_confirmation => "tester"
+em = CardSet.create!(name: "Ember")
+em.cards << Card.new(front: "What are the Ember MVC building blocks?", back: "routes, controllers, templates and views")
+em.cards << Card.new(front: "What is Ember's test data store adapter?", back: "adapter: 'DS.FixtureAdapter'")
+user.card_sets << em
+user.card_sets << CardSet.new(name: "Rails")
+user.save!
