3cefcf028f9fc5f1304c7394d880bf1168a0d667
kagemusha <mmadrid99@gmail.com>
2013-03-14 01:14:46 -0400

    added some cards

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,6 +9,10 @@ user = User.create! :name => "TestUser", :email => "test@test.com", :password =>
 em = CardSet.create!(name: "Ember")
 em.cards << Card.new(front: "What are the Ember MVC building blocks?", back: "routes, controllers, templates and views")
 em.cards << Card.new(front: "What is Ember's test data store adapter?", back: "adapter: 'DS.FixtureAdapter'")
+em.cards << Card.new(front: "What controller property allows it to access another controller?", back: "needs\n\nApp.NeedyController = Em.Controller.extend\n  needs: ['neededController']")
+em.cards << Card.new(front: "What setting allows you to output route transitions", back: "LOG_TRANSITIONS\n\nApp = Ember.Application.create({LOG_TRANSITIONS: true});")
+
+
 user.card_sets << em
 user.card_sets << CardSet.new(name: "Rails")
 user.save!
