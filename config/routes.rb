a3175cf2c97b1533e057034eb801fbe8a1bf2dce
kagemusha <mmadrid99@gmail.com>
2013-05-01 16:25:02 -0400

    removed unnecessary routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,15 +2,11 @@ Crambear::Application.routes.draw do
 
   devise_for :users, controllers: { sessions: 'sessions' }
 
+  #eek fix
   resources :card_sets
   resources :cards
   resources :labels
   resources :results
-  resources :card_sets do
-    resources :cards
-    resources :labels
-  end
-
   root :to => 'home#index'
 
 end
